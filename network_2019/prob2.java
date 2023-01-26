5d9be222120bc49ab36aa912feb6eea0c2f611ec
mio256 <reiya@woorth.io>
2023-01-26 11:18:49 +0900

    :fire: network_2019

--- network_2019/prob2.java
+++ network_2019/prob2.java
@@ -34,7 +34,7 @@ class AirPlaneTripDefault {
 }
 
 class AirPlaneTrip extends AirPlaneTripDefault {
-    String destination="Tokyo";
+    String destination = "Tokyo";
 }
 
 interface DispTicket {
@@ -43,18 +43,18 @@ interface DispTicket {
     void showPeriod();
 }
 
-class OneWay extends AirPlaneTrip implements DispTicket{
+class OneWay extends AirPlaneTrip implements DispTicket {
     public void display() {
         System.out.println("One Way Ticket: ");
-        System.out.println(start+" -> "+destination);
+        System.out.println(start + " -> " + destination);
     }
 }
 
-class RoundTrip extends AirPlaneTrip implements DispTicket{
+class RoundTrip extends AirPlaneTrip implements DispTicket {
     public void display() {
         System.out.println("Round -Trip Ticket: ");
-        System.out.println (start+" -> "+destination);
-        System.out.println(destination+" -> "+start);
+        System.out.println(start + " -> " + destination);
+        System.out.println(destination + " -> " + start);
     }
 
     public void showPeriod() {
@@ -76,15 +76,15 @@ class Person4 {
     }
 
     void printAllNames() {
-        System.out.print(name+":");
-        if(next != null){
+        System.out.print(name + ":");
+        if (next != null) {
             next.printAllNames();
         }
     }
 }
 
 class Ex3k {
-    public static void main (String args[]) throws IOException {
+    public static void main(String args[]) throws IOException {
         Person4 head;
         Person4 kotani4;
         Person4 akira4;
@@ -101,24 +101,24 @@ class Ex3k {
         akira4.setName("Akira");
 
         kotani4 = new Person4(akira4);
-        kotani4.setName ("Nobuta");
+        kotani4.setName("Nobuta");
 
         head = new Person4(kotani4);
 
-        DispTicket [] menu = new DispTicket [2];
+        DispTicket[] menu = new DispTicket[2];
         DispTicket obj0 = new OneWay();
         DispTicket obj1 = new RoundTrip();
         menu[0] = obj0;
         menu[1] = obj1;
 
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Input number 0 (one way ticket) or 1 (roud-trip ticket).");
         String line;
         while ((line = br.readLine()) != null) {
             int menuNum = Integer.parseInt(line);
             menu[menuNum].display();
             menu[menuNum].showPeriod();
-            System.out.print ("Passengers ");
+            System.out.print("Passengers ");
             head.printAllNames();
             System.out.println();
             System.out.println();
@@ -127,3 +127,108 @@ class Ex3k {
         System.out.println("Thank you.");
     }
 }
+
+// import java.io.*;
+
+// class AirPlaneTripDefault {
+//     String start = "Fukuoka";
+
+//     public void showPeriod() {
+//         System.out.println("Regular tickets are available for 1 day!");
+//     }
+// }
+
+// class AirPlaneTrip extends /* here */ {
+//      String destination="Tokyo";
+//  }
+
+// interface DispTicket {
+//     void display();
+
+//     void showPeriod();
+// }
+
+// class OneWay extends /* here */ implements /* here */{
+//      public void display() {
+//          System.out.println("One Way Ticket: ");
+//          System.out.println(start+" -> "+destination);
+//      }
+//  }
+
+// class RoundTrip extends /* here */ implements /* here */{
+//      public void display() {
+//          System.out.println("Round -Trip Ticket: ");
+//          System.out.println (start+" -> "+destination);
+//          System.out.println(destination+" -> "+start);
+//      }
+ 
+//      public void showPeriod() {
+//          System.out.println("Round -Trip tickets are available for 1 week!");
+//      }
+//  }
+
+// class Person4 {
+//     private Person4 next;
+
+//     Person4(Person4 obj) {
+//         next = obj;
+//     }
+
+//     private String name = "";
+
+//     void setName(String nameIn) {
+//         name = nameIn;
+//     }
+
+//     void printAllNames() {
+//          System.out.print(name+":");
+//          if(/* here */){
+//              /* here */
+//          }
+//      }
+// }
+
+// class Ex3k {
+//     public static void main (String args[]) throws IOException {
+//          Person4 head;
+//          Person4 kotani4;
+//          Person4 akira4;
+//          Person4 syuji4;
+//          Person4 sakusya4;
+ 
+//          sakusya4 = new Person4(null);
+//          sakusya4.setName("Sakusya");
+ 
+//          syuji4 = new Person4(sakusya4);
+//          syuji4.setName("Syuji");
+ 
+//          akira4 = new Person4(syuji4);
+//          akira4.setName("Akira");
+ 
+//          kotani4 = new Person4(akira4);
+//          kotani4.setName ("Nobuta");
+ 
+//          head = new Person4(kotani4);
+ 
+//          DispTicket [] menu = new DispTicket [2];
+//          DispTicket obj0 = /* here */;
+//          DispTicket obj1 = /* here */;
+//          menu[0] = obj0;
+//          menu[1] = obj1;
+ 
+//          BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+//          System.out.println("Input number 0 (one way ticket) or 1 (roud-trip ticket).");
+//          String line;
+//          while ((line = br.readLine()) != null) {
+//              int menuNum = Integer.parseInt(line);
+//              menu[menuNum].display();
+//              menu[menuNum].showPeriod();
+//              System.out.print ("Passengers ");
+//              head.printAllNames();
+//              System.out.println();
+//              System.out.println();
+//              System.out.println("Input number 0 or 1.");
+//          }
+//          System.out.println("Thank you.");
+//      }
+// }

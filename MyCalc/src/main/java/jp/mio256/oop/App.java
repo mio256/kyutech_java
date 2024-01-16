e67fe622119e81175584e0672bd0ae36075be940
mio256 <reiya@woorth.io>
2024-01-16 14:15:56 +0900

    :sparkles: MyCalc; setup maven

---
+++ MyCalc/src/main/java/jp/mio256/oop/App.java
@@ -0,0 +1,11 @@
+package jp.mio256.oop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ MyCalc/src/test/java/jp/mio256/oop/AppTest.java
@@ -0,0 +1,20 @@
+package jp.mio256.oop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
